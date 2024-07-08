d6f87eb4347caf5e19b4ea168af7969d2c4acd0c
nisleime <119459202+nisleime@users.noreply.github.com>
2024-07-08 09:41:58 -0300

    Update conexao.php

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,7 +1,7 @@
 ﻿<?php
   $usuarioBD = 'root';
   $senhaBD   = 'Ncm@647534';
-  $servidorBD= 'localhost';
+  $servidorBD= '172.20.0.4';
   //Faz a conexão com o Banco de dados MYSQL
   @$conexao = mysqli_connect($servidorBD,$usuarioBD,$senhaBD,'saw15') or die("Não foi possivel conectar, aguarde um momento");
   mysqli_set_charset($conexao,"utf8mb4");
